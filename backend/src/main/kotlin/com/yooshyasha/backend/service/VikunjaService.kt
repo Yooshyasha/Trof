9622d9948c208350711125cece9a3a1546a24899
Yooshyasha <mejzikrejzi66@gmail.com>
2026-02-28 19:43:42 +0500

    feat

--- backend/src/main/kotlin/com/yooshyasha/backend/service/VikunjaService.kt
+++ backend/src/main/kotlin/com/yooshyasha/backend/service/VikunjaService.kt
@@ -1,7 +1,22 @@
 package com.yooshyasha.backend.service
 
+import com.yooshyasha.backend.config.VikunjaConfig
+import com.yooshyasha.backend.dto.api.*
+import com.yooshyasha.backend.feign.VikunjaClient
 import org.springframework.stereotype.Service
 
 @Service
-class VikunjaService {
+class VikunjaService(
+    private val vikunjaClient: VikunjaClient,
+    private val config: VikunjaConfig
+) {
+    fun createProject(title: String): ProjectResponse {
+        val request = ProjectRequest(title = title)
+        return vikunjaClient.createProject(config.vikunjaAuthorization(), request)
+    }
+
+    fun createTask(projectId: Int, title: String, labels: List<String>): TaskResponse {
+        val request = TaskRequest(title = title, project_id = projectId, labels = labels.map { TaskLabel(it) })
+        return vikunjaClient.createTask(config.vikunjaAuthorization(), request)
+    }
 }
