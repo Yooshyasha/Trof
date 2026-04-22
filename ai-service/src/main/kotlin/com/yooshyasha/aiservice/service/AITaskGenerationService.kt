b01c9242b84a442f815ce1cbe6fc500bd6f2ba46
Yooshyasha <mejzikrejzi66@gmail.com>
2026-04-22 22:05:51 +0500

    ref

--- ai-service/src/main/kotlin/com/yooshyasha/aiservice/service/AITaskGenerationService.kt
+++ ai-service/src/main/kotlin/com/yooshyasha/aiservice/service/AITaskGenerationService.kt
@@ -3,8 +3,6 @@ package com.yooshyasha.aiservice.service
 import com.yooshyasha.aiservice.ai.TaskManagerAgentProvider
 import dto.GeneratedTasksResponse
 import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -12,16 +10,13 @@ import org.springframework.stereotype.Service
 class AITaskGenerationService(
     private val taskManagerAgentProvider: TaskManagerAgentProvider,
 ) {
-    private val mutex = Mutex()
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineExceptionHandler { _, e ->
         logger.error("Exception process ai agent:", e)
     })
 
     fun generation(text: String): Deferred<GeneratedTasksResponse> = scope.async {
-        mutex.withLock {
-            return@async taskManagerAgentProvider.provideAgent().run(text)
-        }
+        return@async taskManagerAgentProvider.provideAgent().run(text)
     }
 
     suspend fun getTaskResult(task: Deferred<GeneratedTasksResponse>): GeneratedTasksResponse? {
